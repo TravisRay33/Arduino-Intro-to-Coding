816b3ca60c4f3f5006474f7ae662f046f3eef809
TravisRay33 <140020128+TravisRay33@users.noreply.github.com>
2023-08-30 09:29:57 -0500

    Add files via upload

---
+++ 01 LEDs and IDE/01_LEDs_IDE.ino
@@ -0,0 +1,24 @@
+/*
+
+Example sketch 01
+
+BLINKING A LED
+
+  Turn an LED on for one second, off for one second,
+  and repeat forever.
+*/
+
+void setup()
+{
+  pinMode(13, OUTPUT); //LED wire is plugged into pin 13
+}
+
+
+void loop()
+{
+  digitalWrite(13, HIGH);   // Turn on the LED
+  delay(1000);              // Wait for one second
+  digitalWrite(13, LOW);    // Turn off the LED
+  delay(1000);              // Wait for one second
+}
+
