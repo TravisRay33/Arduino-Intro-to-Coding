1494c9db1093d366b1e90f27f900bb46a2e0c052
TravisRay33 <140020128+TravisRay33@users.noreply.github.com>
2023-09-25 09:07:26 -0500

    Update 03a_Pot_Buzzer.ino

--- 03 Buttons, Buzzers, Conditional Programming/03a_Pot_Buzzer.ino
+++ 03 Buttons, Buzzers, Conditional Programming/03a_Pot_Buzzer.ino
@@ -3,27 +3,28 @@
 
 #include "Arduino_SensorKit.h"
 
+//Defines do not take up memory like variables. Cannot be changed. 
 #define BUZZER 5
 #define POT A0
 
+//Utilize variable when I need it stored & will change
 int pot_value;
 
 
 void setup() {
   Serial.begin(9600);
-
   pinMode(BUZZER, OUTPUT);
 }
 
 
 void loop() {
-  pot_value = analogRead(POT);
+  pot_value = analogRead(POT);   //Obtain current Pot value
   Serial.print("Pot State is ");
   Serial.println(pot_value);
 
 
-  tone(BUZZER, pot_value);
+  tone(BUZZER, pot_value);  //Plays a tone at Pot value frequency
   delay(500);
-  noTone(BUZZER);
+  noTone(BUZZER);   //Turn off buzzer
   delay(500);
 }
