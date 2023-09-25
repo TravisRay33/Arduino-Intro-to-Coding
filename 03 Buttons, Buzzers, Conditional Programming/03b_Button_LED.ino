9a0ef5cb193252a9b48396661653294d4df62413
TravisRay33 <140020128+TravisRay33@users.noreply.github.com>
2023-09-25 09:09:18 -0500

    Update 03b_Button_LED.ino

--- 03 Buttons, Buzzers, Conditional Programming/03b_Button_LED.ino
+++ 03 Buttons, Buzzers, Conditional Programming/03b_Button_LED.ino
@@ -11,24 +11,22 @@ bool button_state;
 
 void setup() {
   Serial.begin(9600);
-
   pinMode(BUTTON , INPUT);
   pinMode(LED, OUTPUT);
 
-  digitalWrite(LED, LOW);
-
+  digitalWrite(LED, LOW);  //Starts LED Off
 }
 
 void loop() {
-  button_state = digitalRead(BUTTON);
+  button_state = digitalRead(BUTTON);  //Stores button value 1=true=on, 0=false=off
   Serial.print("Button State is ");
   Serial.println(button_state);
 
-  if (button_state == true) {
-    digitalWrite(LED, HIGH);
+  if (button_state == true) {   //If button is pressed
+    digitalWrite(LED, HIGH);    //Turn on LED
   } 
-  else {
-    digitalWrite(LED, LOW);
+  else {                        //Else (button is not pressed)
+    digitalWrite(LED, LOW);     //Turn off LED
   }
 
   delay(100);
